fc8b6b01bccdc86e6bebbb8f690bf769f4951dfe
enesi sefa <enesisefa380@gmail.com>
2022-07-13 21:50:17 +0200

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,42 @@
 import "./style.css";
 
 type Store = {
-  
-    id: number,
-    type: string,
-    name: string,
-    image: string,
-    price: number,
-    discountedPrice: number,
-    dateEntered: string,
-    stock: number
-}
+  id: number;
+  type: string;
+  name: string;
+  image: string;
+  price: number;
+  discountedPrice: number;
+  dateEntered: string;
+  stock: number;
+};
 
 type User = {
-  
-    firstName: string,
-    lastName: string,
-    id: string,
-    password: string,
-    bag: []
-  
-}
+  firstName: string;
+  lastName: string;
+  id: string;
+  password: string;
+  bag: [];
+};
 
 type State = {
-  store : Store[],
-  users: User[]
- }
- let state: State = {
+  store: Store[];
+  users: User[];
+};
+let state: State = {
   store: [],
-  users: []
- }
+  users: [],
+};
 
 function render() {
   // let body = document.querySelector("body")
   header();
-  main()
-  
+  main();
 }
 
-
-
 function header() {
- 
   let app = document.querySelector("#app");
   if (app === null) return;
-  
 
   let headerEl = document.createElement("header");
   headerEl.className = "header";
@@ -61,13 +53,13 @@ function header() {
 
   let liEl = document.createElement("li");
   liEl.className = "list-item";
-  liEl.textContent = "Girls"
+  liEl.textContent = "Girls";
   let liEl2 = document.createElement("li");
   liEl2.className = "list-item";
-  liEl2.textContent = "Guys"
+  liEl2.textContent = "Guys";
   let liEl3 = document.createElement("li");
   liEl3.className = "list-item";
-  liEl3.textContent = "Sale"
+  liEl3.textContent = "Sale";
 
   let headerRightDiv = document.createElement("div");
   headerRightDiv.className = "header-right";
@@ -93,78 +85,74 @@ function header() {
   let spanBagEL = document.createElement("span");
   spanBagEL.className = "material-symbols-outlined";
   spanBagEL.textContent = "shopping_bag";
-  
+
   iconSearchEl.append(spanSearchEl);
   iconPersonEl.append(spanPersonEl);
   iconBagEl.append(spanBagEL);
   ulEl2.append(iconSearchEl, iconPersonEl, iconBagEl);
   navEL2.append(ulEl2);
   headerRightDiv.append(navEL2);
-  
-  ulEL.append(liEl,liEl2,liEl3)
-  navEL.append(ulEL)
- headerLeftDiv.append(h1El,navEL);
+
+  ulEL.append(liEl, liEl2, liEl3);
+  navEL.append(ulEL);
+  headerLeftDiv.append(h1El, navEL);
   headerEl.append(headerLeftDiv, headerRightDiv);
-   app.append(headerEl);
-  
+  app.append(headerEl);
 }
 
-function main(){
+function main() {
   let divApp = document.querySelector("#app");
   if (divApp === null) return;
 
+  let mainEl = document.createElement("main");
 
-  for (let items of state.store){
-  let mainEl = document.createElement("main")
+  let divInsideMainEl = document.createElement("div");
+  divInsideMainEl.className = "div-main";
 
+  let h3El = document.createElement("Home");
+  h3El.textContent = "Home";
 
- let divInsideMainEl = document.createElement("div")
- divInsideMainEl.className = "div-main"
+  let navigation = document.createElement("nav");
+  navigation.className = "navigation";
 
- let h3El = document.createElement("Home")
- h3El.textContent = "Home"
+  let navigationUlEL = document.createElement("ul");
+  navigationUlEL.className = "navigation__list";
 
- let navigation = document.createElement("nav")
- navigation.className = "navigation"
+  for (let items of state.store) {
+    let navigationLiEL = document.createElement("li");
+    navigationLiEL.className = "navigation__list-one-item";
 
+    let imgEL = document.createElement("img");
+    imgEL.className = "images";
+    imgEL.src = items.image;
+    imgEL.alt = "t-shirt";
 
- let navigationUlEL = document.createElement("ul")
- navigationUlEL.className = "navigation__list"
+    let descriptionEl = document.createElement("h4");
+    descriptionEl.textContent = items.name;
 
- let navigationLiEL = document.createElement("li")
- navigationLiEL.className = "navigation__list-one-item"
+    let priceEl = document.createElement("span");
+    priceEl.textContent = String(`£${items.price}`);
 
- let imgEL = document.createElement("img")
- imgEL.src = items.image
- imgEL.alt = "t-shirt"
+    navigationLiEL.append(imgEL, descriptionEl, priceEl);
+    navigationUlEL.append(navigationLiEL);
 
- let descriptionEl = document.createElement("h4")
- descriptionEl.textContent = items.name
+    navigation.append(navigationUlEL);
+  }
 
- let priceEl = document.createElement("span")
- priceEl.textContent =  String(items.price)
+  divInsideMainEl.append(h3El, navigation);
+  mainEl.append(divInsideMainEl);
 
-
- navigationLiEL.append(imgEL,descriptionEl,priceEl)
- navigationUlEL.append(navigationLiEL)
- navigation.append(navigationUlEL)
-
- divInsideMainEl.append(h3El,navigation)
- mainEl.append(divInsideMainEl)
-
- divApp.append(mainEl)
- }
-
+  divApp.append(mainEl);
 }
 
 render();
 
-function getData(){
-  fetch('http://localhost:3005/store')
-    .then(resp => resp.json())
-    .then(data => {state.store = data
-      render()
-    }
-    )}
-
-    getData()
+function getData() {
+  fetch("http://localhost:3005/store")
+    .then((resp) => resp.json())
+    .then((data) => {
+      state.store = data;
+      render();
+    });
+}
+getData();
